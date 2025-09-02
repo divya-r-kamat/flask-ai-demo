34720515b6e51477d6987cf8601d359331dd0a10
divya-r-kamat <divya.r.kamat@gmail.com>
2025-09-02 10:48:32 +0530

    updated app with port

--- app.py
+++ app.py
@@ -210,4 +210,5 @@ asgi_app = WsgiToAsgi(app)
 
 if __name__ == "__main__":
     # dev friendly
-    app.run(debug=True, port=5000)
+    #app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
